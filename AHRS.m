81c4e7995d668eae9123238dd34e07a9db2ba660
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2020-01-30 20:54:50 +0900

    Add files via upload

---
+++ AHRS.m
@@ -0,0 +1,32 @@
+% AHRS EBIMU 9DOF V4 for raspberry pi
+% This code is written by seong won lee
+clear; clc;
+ipadress='192.168.137.154'; %ip adress Raspberry pi
+mypi=raspi(ipadress,'pi','robotics'); %raspi obeject consturct
+IMU=serialdev(mypi,'/dev/ttyUSB0'); %AHRS Serial Port
+IMUdata=[];
+readdata="";
+  for i=1:50
+        data="";
+        while(readdata~='*')
+            readdata= read(IMU,1,'char');
+        end
+        readdata= read(IMU,1,'char');
+        data=data+readdata;
+        while(readdata~="*")
+             readdata= read(IMU,1,'char');
+             data=data+readdata;
+        end            
+        % dataÁ¤¸®
+        splitdata=strsplit(data,',');
+        splitdata{6} = splitdata{6}(1:length(splitdata{6})-3);
+        c=clock;
+        IMUdata(1:6)=c;
+        IMUdata(7)=str2double(splitdata{1});
+        IMUdata(8)=str2double(splitdata{2});
+        IMUdata(9)=str2double(splitdata{3});
+        IMUdata(10)=str2double(splitdata{4});
+        IMUdata(11)=str2double(splitdata{5});
+        IMUdata(12)=str2double(splitdata{6});
+        pause(0.001)
+end
